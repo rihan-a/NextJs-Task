2eca5141af75836f36a9f33e3ad47685d7d02002
rihan_a <a.rihan@live.com>
2023-09-02 21:19:47 +0300

    store selected category id in a state

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,13 @@ export default function Home() {
   const [categories, setCategories] = useState([]);
   const [parentCategoryId, setParentCategoryId] = useState(0);
 
+
+  const handleMainCategory = (event) => {
+    setParentCategoryId(event.target.value);
+    console.log(event.target.value);
+  };
+
+
   // fetch categories data 
   useEffect(() => {
     async function fetchCategories() {
@@ -28,16 +35,20 @@ export default function Home() {
       <div className={styles.mainContainer}>
         <h2>Multi-Level Forms task:</h2>
         <h4>Parent Category</h4>
-        <select value={parentCategoryId} className={styles.select}>
+
+        <select className={styles.select} value={parentCategoryId} onChange={handleMainCategory}>
           <option>Select Parent Category</option>;
           {categories.map((category) => {
             return <option key={category.id} value={category.id}>{category.name}</option>;
           })}
         </select>
+
         <h4>Sub Category</h4>
+
         <select className={styles.select}>
           <option>Select Sub-category</option>
         </select>
+
       </div>
     </main>
   );
