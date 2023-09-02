053805447dfff5cc5f75d4308d928e48f005aeba
rihan_a <a.rihan@live.com>
2023-09-02 20:55:15 +0300

    create select tags and titles

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,17 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <div className={styles.mainContainer}>
+        <h2>Multi-Level Forms task:</h2>
+        <h4>Parent Category</h4>
+        <select className={styles.select}>
+          <option>Select Parent Category</option>
+        </select>
+        <h4>Sub Category</h4>
+        <select className={styles.select}>
+          <option>Select Sub-category</option>
+        </select>
+      </div>
     </main>
   );
 }
